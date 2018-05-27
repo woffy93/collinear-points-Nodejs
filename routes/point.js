8417b3774f0d98d6085e1007ce76c922b6978907
Francesco Wofford <francescowofford@gmail.com>
2018-05-27 17:42:10 +0200

    fixes

--- routes/point.js
+++ routes/point.js
@@ -9,8 +9,10 @@ router.post('/', function (req, res) {
     if (typeof req.body.x ==="number" && typeof req.body.x ==="number") {
         let point = new ds.Point(req.body.x,req.body.y);
         algorithm.addPt(point);
+        res.send("ok");
     }
     else res.send("error");
+
 });
 router.get('/', function (req, res) {
     res.send("ok");
